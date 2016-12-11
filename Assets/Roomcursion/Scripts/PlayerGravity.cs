b5b2ce9b153b0ce55cac1b4c0bb1d9523f0e3558
Austin Spafford <austin.spafford@gmail.com>
2016-12-10 16:27:53 -0800

    Making the player fall to ground-level, and tweaking materials

---
+++ Assets/Roomcursion/Scripts/PlayerGravity.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerGravity : MonoBehaviour
+{
+	public float LocalGroundY = 0.0f;
+	public float GravityScalar = 1.0f;
+
+	public void Update()
+	{
+		if (transform.localPosition.y > LocalGroundY)
+		{
+			localVelocity += (Physics.gravity.y * Time.deltaTime * GravityScalar);
+			
+			Vector3 localPosition = transform.localPosition;
+			localPosition.y += (localVelocity * Time.deltaTime);
+			transform.localPosition = localPosition;
+		}
+
+		if (transform.position.y <= LocalGroundY)
+		{
+			localVelocity = 0.0f;
+
+			Vector3 localPosition = transform.localPosition;
+			localPosition.y = LocalGroundY;
+			transform.localPosition = localPosition;
+		}
+	}
+
+	private float localVelocity = 0.0f;
+}
